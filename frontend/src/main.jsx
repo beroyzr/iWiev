f104fe9949f33932ca0ffdaf40095531203d43fc
berat yazır <beroyzr5@gmail.com>
2024-11-21 00:19:15 +0300

    kkk

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 import './index.css'
+import './store/axios.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
